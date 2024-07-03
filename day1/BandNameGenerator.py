fe0bf7b2236b9e64f845ee0d68e6505b4fe5b70a
Uditarko <uditarko.dutta@gmail.com>
2024-07-04 00:09:39 +0530

    band name generator

---
+++ day1/BandNameGenerator.py
@@ -0,0 +1,10 @@
+print("Welcome to Band Name Generator\n")
+
+print("Please enter your city name\n")
+city_name = input()
+
+print("Please enter your pet name\n")
+pet_name = input()
+
+band_name = city_name+" "+pet_name
+print("Generated Band NAme is : "+band_name+"\n")
